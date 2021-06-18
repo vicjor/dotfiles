54b6e3993568559c6b0d9b0a05239ccd285427dc
Victor Jørgensen <victor.jorgens@gmail.com>
2021-06-18 14:59:38 +0200

    Add more macOS applications to be install with cask

--- brew.sh
+++ brew.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew upgrade
+
 # Install Brew Packages
 brew install python
 brew install tree
 
 # Install MacOS Applications
-brew cask install google-chrome
-brew cask install sourcetree
-brew cask install spotify
-brew cask install google-backup-and-sync
-brew cask install vlc
+brew install --cask google-chrome
+brew install --cask spotify
+brew install --cask visual-studio-code
+brew install --cask vim
+brew install --cask wget
+brew install --cask iterm2
+brew install --cask messenger
+brew install --cask spectacle
+brew install --cask 1password
+brew install --cask miro
+brew install --cask postman
+brew install --cask notion
+brew install --cask gitkraken
+brew install --cask slack
